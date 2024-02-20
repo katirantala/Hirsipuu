90f914173c5136472a677c9bd1175f5f38152dc3
katirantala <n3raka00@students.oamk.fi>
2024-02-20 14:52:43 +0200

    Hirsipuu valmis.

---
+++ js/functions.js
@@ -0,0 +1,69 @@
+const input = document.querySelector('input')
+const output = document.querySelector('output')
+const span = document.querySelector('span')
+let count_guess = 0
+
+const words = [
+    "programming",
+    "javascript",
+    "database",
+    "markup",
+    "framework",
+    "variable",
+    "stylesheet",
+    "library",
+    "asynchronous",
+    "hypertext"
+]
+
+let randomizedWord = ''
+let maskedWord = ''
+
+const newGame = () => {
+    const random = Math.floor(Math.random() * 10) + 1
+    randomizedWord = words [random]
+    maskedWord = "*".repeat(randomizedWord.length)
+    console.log(randomizedWord)
+    output.innerHTML =maskedWord
+}
+const win = () => {
+    alert(`You have guessed right, the word is ${randomizedWord}. You guessed ${count_guess} times.`)
+    newGame()
+}
+
+const replaceFoundChars = (guess) => {
+    for (let i = 0;i<randomizedWord.length;i++) {
+        const char = randomizedWord.substring(i,i+1)
+        if (char === guess) {
+            let newString = maskedWord.split('')
+            newString.splice(i,1,guess)
+            newString = newString.join('')
+            maskedWord = newString
+        }
+    }
+    output.innerHTML = maskedWord
+}
+
+newGame()
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+
+        const guess = input.value
+        if (guess.toLowerCase() === randomizedWord.toLowerCase()) {
+            win()
+        } else if (guess.length === 1) {
+            replaceFoundChars(guess)
+            if (maskedWord.toLocaleLowerCase() === randomizedWord.toLocaleLowerCase()) {
+                win()
+            }
+        } else {
+            alert("You guessed wrong!")
+        }
+        input.value = ''
+    }
+    count_guess++
+    span.innerHTML = count_guess
+})
+
